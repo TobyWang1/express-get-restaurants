64ed0ba81d6bbf97ae4b451dec47b46dcac0be7a
TobyWang1 <fufummumm@gmail.com>
2024-10-14 13:56:15 +0100

    Add HTTP get method to retrieve all restaurans data in JSON format'

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
-
-
+app.get('/restaurants', async (request, response) => {
+    const restaurants = await Restaurant.findAll()
+    app.send(response.json(restaurants))
+})
 
 module.exports = app;
